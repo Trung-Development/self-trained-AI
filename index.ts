5534c631c862813ae89a6167d7719e3df465d9f0
Vũ Thành Trung <vuthanhtrungsuper@gmail.com>
2024-11-28 10:57:08 +0000

    Update server port configuration to use PORT variable

--- index.ts
+++ index.ts
@@ -553,6 +553,6 @@ app.get('/', (req, res) => {
     `);
 });
 
-app.listen(3000, () => {
-    console.log(`Express server listening on port 3000`);
+app.listen(PORT, () => {
+    console.log(`Express server listening on port ${PORT}`);
 });
